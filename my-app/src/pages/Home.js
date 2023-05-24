8cb9260eed49a02ddcc0bb411ef4f1e0d16e1cad
wento <florea_andreiwento@yahoo.com>
2023-05-24 17:52:28 -0500

    added new components, new cod in pages file ,fixed  app.js

---
+++ my-app/src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Nav from "../components/nav";
+import Menu from "../components/menu";
+
+const Home = () => {
+  return (
+    <div className="container">
+      <Nav />
+      <Menu />
+    
+    </div>
+  );
+};
+
+export default Home;
